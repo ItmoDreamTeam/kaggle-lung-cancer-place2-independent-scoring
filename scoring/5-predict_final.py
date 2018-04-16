f49ea24f4325741c8f7b4e8ea131eef56de0382f
Kirill Smirnov <smirnowku@gmail.com>
2018-04-16 22:53:09 +0300

    refactor step5

--- scoring/5-predict_final.py
+++ scoring/5-predict_final.py
@@ -1,14 +1,17 @@
+import settings
 import pandas as pd
-import numpy as np
+
+ENSEMBLE1_DIR = settings.TMP_DIR + '/ensemble1'
+ENSEMBLE2_DIR = settings.TMP_DIR + '/ensemble2'
 
 if __name__ == '__main__':
-    np.random.seed(42)
-    dh_ens1 = pd.read_csv("ens1_preds_stage2.csv")
-    dh_ens2 = pd.read_csv("ens2_preds_stage2.csv")
-    jul_preds_test = pd.read_csv("julian_preds_test.csv")
-    df = pd.merge(left=jul_preds_test, right=dh_ens1, how='inner', left_on='patient_id', right_on='id')
+    dh_ens1 = pd.read_csv(ENSEMBLE1_DIR + '/predictions.csv')
+    dh_ens2 = pd.read_csv(ENSEMBLE2_DIR + '/predictions.csv')
+
+    df = dh_ens1
     df = pd.merge(left=df, right=dh_ens2, how='inner', left_on='patient_id', right_on='id')
-    df['cancer'] = 0.4 * df['yh_jul'] + 0.6 * (0.7 * df['yh_ens1'] + 0.3 * df['yh_ens2'])
+
+    df['cancer'] = 0.7 * df['yh_ens1'] + 0.3 * df['yh_ens2']
     df['id'] = df['patient_id']
     df = df[['id', 'cancer']]
-    df.to_csv('final_predictions_dh_blend.csv', index=False)
+    df.to_csv(settings.TMP_DIR + '/final_predictions.csv')
