1c7b7f0555d261a269003897a9fac11ffaa48179
Kirill Smirnov <smirnowku@gmail.com>
2018-04-16 22:06:39 +0300

    refactor step3.2

--- scoring/3.2-score_ensemble2.py
+++ scoring/3.2-score_ensemble2.py
@@ -1,13 +1,19 @@
 import settings
 import os
+import sys
+import numpy as np
 import pandas as pd
+from scipy import ndimage
+from sklearn.cluster import DBSCAN
+from keras.models import load_model
+from multiprocessing import Pool
 
 INPUT_DIR = settings.TMP_DIR + '/v1_nodules'
 OUTPUT_DIR = settings.TMP_DIR + '/ensemble2'
 
 
 def random_perturb(Xbatch, rotate=False):
-    # apply some random transformations...
+    # apply some random transformations
     swaps = np.random.choice([-1, 1], size=(Xbatch.shape[0], 3))
     Xcpy = Xbatch.copy()
     for i in range(Xbatch.shape[0]):
@@ -41,7 +47,7 @@ def get_loc_features(locs, malig_scores, sizes):
     num_clusters = np.max(dbs.labels_) + 1
     num_noise = (dbs.labels_ == -1).sum()
 
-    # new feature: sum of malig_scores but normalizing by cluster.
+    # new feature: sum of malig_scores but normalizing by cluster
     cluster_avgs = []
     for clusternum in range(num_clusters):
         cluster_avgs.append(malig_scores[dbs.labels_ == clusternum].mean())
@@ -51,7 +57,6 @@ def get_loc_features(locs, malig_scores, sizes):
         if clusterix == -1:
             cluster_avgs.append(malig)
 
-    weighted_sum_malig = np.sum(cluster_avgs)
     weighted_mean_malig = np.mean(cluster_avgs)
 
     # size of biggest cluster
@@ -70,21 +75,16 @@ def get_loc_features(locs, malig_scores, sizes):
 def process_voxels(model, voxels, locs, sizes):
     # mapping from the set of voxels for a patient to a set of features
     n_TTA = 10
-    preds = []
-
-    preds.append(np.concatenate(model.predict(voxels, batch_size=64), axis=1))
+    preds = list(np.concatenate(model.predict(voxels, batch_size=64), axis=1))
 
     # parallelism for data augmentation
-    # pvoxels = Parallel(n_jobs=4, verbose=0)(delayed(random_perturb)(voxels, True) for i in range((n_TTA)*len(models)))
-    # ix = 0
     for i in range(n_TTA):
         preds.append(np.concatenate(model.predict(random_perturb(voxels, i < 4), batch_size=64), axis=1))
 
     preds = np.stack(preds)  # axis0 is over each TTA
-    Xmean = np.mean(preds, axis=0)  # mean over tta, still have a set of predictions per voxel.
-    # Xdev = np.std(preds, axis=0) #std over tta. one set of SDs per voxel per feature.
+    Xmean = np.mean(preds, axis=0)  # mean over tta, still have a set of predictions per voxel
+    Xcomb = Xmean
 
-    Xcomb = Xmean  # (Xmean - Xdev) #higher SD -> lower value. TODO: experiment
     # axis 0 is per voxel, axis 1 is per feature
     # max and sum.
     xmax = np.max(Xcomb, axis=0)
@@ -96,35 +96,22 @@ def process_voxels(model, voxels, locs, sizes):
 
 def score_model(modelpath, name):
     # score a model on the train and test set
-    from keras.models import load_model
     model = load_model(modelpath)
-
     files = [f.replace('vox_', '') for f in os.listdir(INPUT_DIR) if 'vox_' in f]
-    all_features = []
-    # all_targets = []
-    for i, patient in enumerate(files):
-        # print f
-        # filter = (patients == patient)
 
+    all_features = []
+    for i, patient in enumerate(files):
         patient_vox = np.load(os.path.join(INPUT_DIR, 'vox_' + patient))  # voxels[filter]
         patient_locs = np.load(os.path.join(INPUT_DIR, 'cents_' + patient))  # locations[filter]
         patient_sizes = np.load(os.path.join(INPUT_DIR, 'shapes_' + patient))  # sizes[filter]
-        patient_nodule_preds = np.load(os.path.join(INPUT_DIR, 'preds_' + patient))
-        # TODO: try different thresholds.
+        print patient_vox.shape[0], 'nodules for patient', patient, 'number', i + 1, 'of', len(files), 'model', name
 
-        print patient_vox.shape[0], 'nodules for patient', patient, 'number', i, 'of', len(files), 'model', name
         features = process_voxels(model, patient_vox, patient_locs, patient_sizes)
-        # target = get_target(patient)
-
         all_features.append(features)
-        # all_targets.append(target)
-
         X = np.stack(all_features)
-        # F = np.array(files)
 
     df = pd.DataFrame(data=X, index=files)
-
-    df.to_csv(name + '.csv')
+    df.to_csv(OUTPUT_DIR + '/' + name + '.csv')
     return
 
 
@@ -133,55 +120,22 @@ def score_wrapper(args):
 
 
 if __name__ == '__main__':
-    from scipy import ndimage
-    import numpy as np
-    import os
-    import pdb
-    import cPickle
-    # from keras.models import load_model
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.metrics import log_loss
-    import sys
-    import scipy
-    from sklearn.cluster import DBSCAN
-    from joblib import Parallel, delayed
-
     sys.setrecursionlimit(10000)
 
-    import os
+    model37 = settings.MODEL_DIR + '/ensemble2/model_des_v37_mse_64_finetune_04.h5'
+    model37b = settings.MODEL_DIR + '/ensemble2/model_des_v37b_mse_64_finetune_04.h5'
+    model37c = settings.MODEL_DIR + '/ensemble2/model_des_v37c_64_finetune_04.h5'
+    model37d = settings.MODEL_DIR + '/ensemble2/model_des_v37d_64_finetune_04.h5'
+    model37f = settings.MODEL_DIR + '/ensemble2/model_des_v37f_64_finetune_04.h5'
+    model37g = settings.MODEL_DIR + '/ensemble2/model_des_v37g_64_finetune_04.h5'
+    model38 = settings.MODEL_DIR + '/ensemble2/model_des_v38_mse_64_finetune_04.h5'
 
-    # load the models
-    if True:
-        model37g = "ensemble2/model_des_v37g_64_finetune_04.h5"
-        print '37g'
-        model37b = "ensemble2/model_des_v37b_mse_64_finetune_04.h5"
-        print 'loaded 37b'
-        model37d = "ensemble2/model_des_v37d_64_finetune_04.h5"
-        print '37d'
-        model37f = "ensemble2/model_des_v37f_64_finetune_04.h5"
-        print '37f'
-        model38 = "ensemble2/model_des_v38_mse_64_finetune_04.h5"
-        print '38'
-        # from local
-        model37 = "ensemble2/model_des_v37_mse_64_finetune_04.h5"
-        print '37'
-        model37c = "ensemble2/model_des_v37c_64_finetune_04.h5"
-        print '37c'
+    models = [model37, model37b, model37c, model37d, model37f, model38, model37g]
+    names = ['37', '37b', '37c', '37d', '37f', '38', '37g']
+    names = ['model_features_' + n for n in names]
 
-        models = [model37, model37b, model37c, model37d, model37f, model38, model37g]
-        names = ['37', '37b', '37c', '37d', '37f', '38', '37g']
-        names = ['model_features_stage2_' + n for n in names]
-    else:
-        # model37c = load_model(os.path.join(DATA_DIR, "code", "models", "model_des_v37c_64_finetune_04.h5"))
-        print '37c'
-        models = ['ensemble2/model_des_v37c_64_finetune_04.h5']  # model37c, model37c]
-        names = ['test1']  # , 'test2']
-
-    # from joblib import Parallel,delayed
-    # score_model(models[0], names[0])
-    # score_model(models[1],names[1])
-    # Parallel(n_jobs=2)(delayed(score_model)(model,name) for model,name in zip(models,names))
-    from multiprocessing import Pool
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
 
     pool = Pool(7)
     args = zip(models, names)
